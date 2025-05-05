964009aafdfe604020fd229a5887639edd78175d
negarak98 <negar.khatamian@yahoo.com>
2025-05-05 19:46:23 -0400

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "mcitstoragemay" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.may022025.name
-  location                 = azurerm_resource_group.may022025.location
+  resource_group_name      = azurerm_resource_group.terraformmay022025.name
+  location                 = azurerm_resource_group.terraformmay022025.location
   account_tier             = "standard"
   account_kind             ="files storage"
   account_replication_type = "LRS"
