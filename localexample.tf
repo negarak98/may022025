41f86c28eca8fe952904c4c32c55ca29771a7f62
negarak98 <negar.khatamian@yahoo.com>
2025-05-07 19:12:32 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -6,6 +6,6 @@ locals{
     total_output = ["150", "150", "150"]
     characters = ["luke", "yoda", "darth"]
 bestrestaurants=["timeoutmarket","lecarre","iledefrance"]
-  total_output = ["150", "150", "150"]
+  
   }
 
