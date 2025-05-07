0bb4988d19dd5112f6d8262c34899d6cdc71e3fe
negarak98 <negar.khatamian@yahoo.com>
2025-05-07 17:32:46 -0400

    Update storagecontainer.tf

--- storagecontainer.tf
+++ storagecontainer.tf
@@ -1,4 +1,4 @@
-  recource "azurerm_storage_container" "mcitcontainermay"  {
+  resource "azurerm_storage_container" "mcitcontainermay"  {
   name                                = "content"
   storage_account_id                  = azurerm_storage_account.example.id
   container_access_type               = "private"
