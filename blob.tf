67a8023f02d088d5f1183a964fb9603f963e291a
negarak98 <negar.khatamian@yahoo.com>
2025-05-07 17:30:19 -0400

    Update blob.tf

--- blob.tf
+++ blob.tf
@@ -1,1 +1,6 @@
-
+resource "azurerm_storage_blob" "mcitstoragemay" {
+  name                          = "my-awesome-content.zip"
+  storage_account_name          = azurerm_storage_account.example.name  
+  storage_container_name        = azurerm_storage_container.example.name
+  type                           = "Block"
+  }
