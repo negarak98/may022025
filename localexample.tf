772bb85e30e38ab291904e90be7d7af8a201c598
negarak98 <negar.khatamian@yahoo.com>
2025-05-07 18:55:26 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -2,19 +2,5 @@ locals{
   servicename= "negarak"
   forum= "negarakmcit"
   lengthsa=length(local.servicename)
-
-}
-variable "winter_list_of_sports" {
-  type    = list(string)
-  default = ["icehockey", "snowboarding", "iceskating"]
-}
-
-variable "total_output" {
-  type    = list(string)
-  default = ["150", "150", "150"]
-}
-
-variable "characters" {
-  type    = list(string)
-  default = ["luke", "yoda", "darth"]
+    winterlistofsports=["icehockey","snowboarding","iceskating"]
 }
