fea3912c624a41b0adc2dbac9cb30c72faa34218
negarak98 <negar.khatamian@yahoo.com>
2025-05-07 18:46:25 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -29,3 +29,19 @@ variable "environment"{
   type=string
   default="prod"
 }
+
+
+ variable "winter_list_of_sports" {
+  type    = list(string)
+  default = ["icehockey", "snowboarding", "iceskating"]
+}
+
+variable "total_output" {
+  type    = list(string)
+  default = ["150", "150", "150"]
+}
+
+variable "characters" {
+  type    = list(string)
+  default = ["luke", "yoda", "darth"]
+}
