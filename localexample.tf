40d408844c54e49fabe9b62a95638de8c6e3be45
negarak98 <negar.khatamian@yahoo.com>
2025-05-07 19:43:11 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -4,12 +4,9 @@ locals{
   lengthsa=length(local.servicename)
     winterlistofsports=["icehockey","snowboarding","iceskating"]
     total_output = ["150", "150", "150"]
-    
-bestrestaurants=["timeoutmarket","lecarre","iledefrance"]
-
-characters = ["luke", "yoda", "darth"]
-  enemies_destroyed = [4252, 900, 20000056894]
-character_enemy_map =   { for character in local.characters: # Convert character list to a set
+    characters = ["luke", "yoda", "darth"]
+    enemies_destroyed = [4252, 900, 20000056894]
+    character_enemy_map =   { for character in local.characters: # Convert character list to a set
       character => local.enemies_destroyed
 }
   
