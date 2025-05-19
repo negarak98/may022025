6cda54f791f0c080232a49f1e164c889003f0a51
negarak98 <negar.khatamian@yahoo.com>
2025-05-19 18:15:11 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -19,3 +19,18 @@ character_food_map= {for character in local.bestrestaurants: # Convert character
   }
 }
 
+locals{
+  quote = var.quote
+
+  upper_quote     = upper(local.quote)
+  lower_quote     = lower(local.quote)
+  short_quote     = substr(local.quote, 0, 10)
+  reversed        = join("", reverse(split("", local.quote)))
+  replaced        = replace(local.quote, "potholes", "hotdogs")
+  has_zip         = can(regex("zip", local.quote))
+  other_quotes    = ["Toronto: City of condos.", "Vancouver: Rain and real estate."]
+  joined_quotes   = join(" | ", concat(local.other_quotes, [local.quote]))
+  words           = split(" ", local.quote)
+  quote_length    = length(local.quote)
+  branded_quote   = "${local.quote} #TrueStory"
+}
