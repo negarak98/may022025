001cd4b337b5e3c8c608b4f788cd6fdc442e6f69
negarak98 <negar.khatamian@yahoo.com>
2025-05-24 16:51:14 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -33,20 +33,6 @@ output "character_enemy_output" {
 }
 
 
-output "string_manipulations" {
-  value = {
-    upper          = local.upper_quote
-    lower          = local.lower_quote
-    first_10_chars = local.short_quote
-    reversed       = local.reversed
-    replaced       = local.replaced
-    contains_zip   = local.has_zip
-    joined         = local.joined_quotes
-    split_words    = local.words
-    length         = local.quote_length
-    with_suffix    = local.branded_quote
-  }
-}
 
 
   output "reverse_sentence_1" {
