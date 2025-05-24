af945db25c69da5aa3a9a18fee3bd4647442791b
negarak98 <negar.khatamian@yahoo.com>
2025-05-24 16:49:30 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -39,38 +39,3 @@ locals{
 
 
 
-locals {
-  # Use the input variable
-  quote = var.quote
-
-  # 1. Convert to Uppercase
-  upper_quote = upper(local.quote)
-
-  # 2. Convert to Lowercase
-  lower_quote = lower(local.quote)
-
-  # 3. Get First 10 Characters
-  short_quote = substr(local.quote, 0, 10)
-
-  # 4. Reverse the String
-  reversed = join("", reverse(split("", local.quote)))
-
-  # 5. Replace "potholes" with "hotdogs"
-  replaced = replace(local.quote, "potholes", "hotdogs")
-
-  # 6. Check if it Contains "zip"
-  has_zip = can(regex("zip", local.quote))
-
-  # 7. Join with a List of Other City Quotes
-  other_quotes = ["Toronto: City of condos.", "Vancouver: Rain and real estate."]
-  joined_quotes = join(" | ", concat(local.other_quotes, [local.quote]))
-
-  # 8. Split the Quote by Spaces
-  words = split(" ", local.quote)
-
-  # 9. Length of the Quote
-  quote_length = length(local.quote)
-
-  # 10. Add a Suffix to the Quote
-  branded_quote = "${local.quote} #TrueStory"
-}
