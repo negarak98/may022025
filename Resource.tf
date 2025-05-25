a0d8b8ee5a4bbb00ccd3fff99cc214a957469267
negarak98 <negar.khatamian@yahoo.com>
2025-05-25 18:18:57 -0400

    Update Resource.tf

--- Resource.tf
+++ Resource.tf
@@ -1,14 +1,14 @@
-resource "azurerm_resource_group" "azureresourcegroup"{
+resource "azurerm_resource_group" "mcitdevrm"{
   name  = "mcit_resource_group_may022025"
   location="canadacentral"
   }
 
 
-resource "azurerm_service_plan" "azureresourcegroup" {
+resource "azurerm_service_plan" "mcitdevrm" {
   for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.mcitdevrm.name
+  location            = azurerm_resource_group.mcitdevrm.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
 }
@@ -16,8 +16,8 @@ resource "azurerm_service_plan" "azureresourcegroup" {
 resource "azurerm_linux_web_app" "azureresourcegroupwebapp" {
   for_each            = azurerm_service_plan.azureresourcegroup
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.mcitdevrm.name
+  location            = azurerm_resource_group.mcitdevrm.location
   service_plan_id     = each.value.id
 
   site_config {}
