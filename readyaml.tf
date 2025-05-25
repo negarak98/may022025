4d9f569477680ea0caee242ca89d96ea5c2a4d43
negarak98 <negar.khatamian@yahoo.com>
2025-05-25 15:02:51 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -1,6 +1,6 @@
 locals {  
 # get list of YAML files in the CONFIGS folder 
-linux_app_files = fileset("${path.module}/configs" , "[^_]*.yaml")
+  linux_app_files = fileset("${path.module}/configs" , "[^_]*.yaml")
 }
 
 #read and decode each YAML file
@@ -8,3 +8,14 @@ linux_app_files = fileset("${path.module}/configs" , "[^_]*.yaml")
     for f in local.linux_app_files :
     yamldecode(file("${path.module} /configs/${f}"))
 }
+
+#extract app info from file
+  linux_app_list = flatten([ for app in local.linux_app_data : [ for linuxapp in try(app/listoflinuxapp,  [] : {
+  name     = linuxapp.name
+  os_type  = linuxapp.os_type
+  sku_name = linuxapp.sku_name
+}
+]
+])
+}
+  
