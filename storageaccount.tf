7eced8850f47dc7c5c50d33a7f48f565dfd53fdf
negarak98 <negar.khatamian@yahoo.com>
2025-05-25 18:19:51 -0400

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "mcitstoragemay" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.azureresourcegroup.name
-  location                 = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name      = azurerm_resource_group.mcitdevrm.name
+  location                 = azurerm_resource_group.mcitdevrm.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
