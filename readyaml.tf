f000875b0c38b69a353189d358653c4fa402c853
negarak98 <negar.khatamian@yahoo.com>
2025-05-25 17:21:11 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -14,7 +14,7 @@ locals {
 locals {
 #extract app info from file #flatten is a function that turns a list of lists into one list.
 
-  linux_app_list = flatten([ for app in local.linux_app_data : [ for linuxapp in try(app/listoflinuxapp,  []) : {
+  linux_app_list = flatten([ for app in local.linux_app_data : [ for linuxapp in try(app.listoflinuxapp,  []) : {
   name     = linuxapp.name
   os_type  = linuxapp.os_type
   sku_name = linuxapp.sku_name
