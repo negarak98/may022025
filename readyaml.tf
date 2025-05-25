ed872d64b4e66095be436b24a17d8aeec59bb3cd
negarak98 <negar.khatamian@yahoo.com>
2025-05-25 15:21:28 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -3,13 +3,19 @@ locals {
   linux_app_files = fileset("${path.module}/configs" , "[^_]*.yaml")
 }
 
+
+
 #read and decode each YAML file
   linux_app_data = { 
     for f in local.linux_app_files :
     yamldecode(file("${path.module} /configs/${f}"))
+#means read YAML text(file) and turns into data that terraform can use(yamldecode)
 }
 
-#extract app info from file
+
+
+#extract app info from file #flatten is a function that turns a list of lists into one list.
+
   linux_app_list = flatten([ for app in local.linux_app_data : [ for linuxapp in try(app/listoflinuxapp,  [] : {
   name     = linuxapp.name
   os_type  = linuxapp.os_type
@@ -18,4 +24,5 @@ locals {
 ]
 ])
 }
+
   
