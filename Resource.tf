1b306cb1c19ec869f21f3058837703928c81c1bf
negarak98 <negar.khatamian@yahoo.com>
2025-05-25 16:59:46 -0400

    Update Resource.tf

--- Resource.tf
+++ Resource.tf
@@ -2,3 +2,23 @@ resource "azurerm_resource_group" "terraformmay022025"{
   name  = "mcit_resource_group_may022025"
   location="canadacentral"
   }
+
+
+resource "azurerm_service_plan" "batcha06sp" {
+  for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  os_type             = each.value.os_type
+  sku_name            = each.value.sku_name
+}
+
+resource "azurerm_linux_web_app" "batcha06webapp" {
+  for_each            = azurerm_service_plan.batcha06sp
+  name                = each.value.name
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  location            = azurerm_resource_group.azureresourcegroup.location
+  service_plan_id     = each.value.id
+
+  site_config {}
+}
