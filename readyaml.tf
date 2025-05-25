8553fc836f11caa532e6c98ea5e29d8242584e34
negarak98 <negar.khatamian@yahoo.com>
2025-05-25 18:44:40 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -1,11 +1,11 @@
 locals {  
 # get list of YAML files in the CONFIGS folder 
-  linux_app_files = fileset("${path.module}/configs" , "[^_]*.yaml")
+  windows_app_files = fileset("${path.module}/configs" , "[^_]*.yaml")
 
 
 #read and decode each YAML file
-  linux_app_data = { 
-    for f in local.linux_app_files :
+  windows_app = { 
+    for windowsapp in local.linux_app_files :
    f => yamldecode(file("${path.module}/configs/${f}"))
 #means read YAML text(file) and turns into data that terraform can use(yamldecode)
 }
@@ -13,7 +13,7 @@ locals {
 
 #extract app info from file #flatten is a function that turns a list of lists into one list.
 
-  linux_app_list = flatten([ for app in local.windows_app : [ for linuxapp in try(app.listofwindowsapp,  []) : {
+  windows_app_list = flatten([ for app in local.windows_app : [ for linuxapp in try(app.listofwindowsapp,  []) : {
   name     = windowsapp.name
   os_type  = windowsapp.os_type
   sku_name = windowsapp.sku_name
