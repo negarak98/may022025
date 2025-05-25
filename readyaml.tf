de87c6bed07303739f47524f4e2007b3b989c3f3
negarak98 <negar.khatamian@yahoo.com>
2025-05-25 14:55:10 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -1,1 +1,10 @@
+locals {  
+# get list of YAML files in the CONFIGS folder 
+linux_app_files = fileset("${path.module}/configs" , "[^_]*.yaml")
+}
 
+#read and decode each YAML file
+  linux_app_data = { 
+    for f in local.linux_app_files :
+    yamldecode(file("${path.module} /configs/${f}"))
+}
