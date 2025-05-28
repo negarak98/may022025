377db10d1a9ddacaf453d8c333ca984f331ac619
negarak98 <negar.khatamian@yahoo.com>
2025-05-28 18:56:49 -0400

    Update localexample.tf

--- localexample.tf
+++ localexample.tf
@@ -115,3 +115,10 @@ locals {
 locals {
   cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
+
+locals {
+  subnet_data = yamldecode(file("${path.module}/subnets.yaml"))
+  subnets     = local.subnet_data.subnets
+}
+
+
